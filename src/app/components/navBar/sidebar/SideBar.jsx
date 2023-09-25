0b811976c13e36dd3cba9b29f347e5230e85afef
Harsh <hgaur491@gmail.com>
2023-09-25 14:31:56 +0530

    ///// initial changes

--- src/app/components/navBar/sidebar/SideBar.jsx
+++ src/app/components/navBar/sidebar/SideBar.jsx
@@ -1,33 +1,38 @@
 import Link from "next/link";
 import "./sideBar.css"
-
+import Icons from "../../Icons/Icon";
 export default function SideNavBar() {
     return (
         <header className="side-navbar-header">
             <nav className='side-navbar'>
                 <ul className='nav-links'>
-                    <li className="side-toggle-btn"><i class="fa-solid fa-bars"></i></li>
+                    <li className="side-toggle-btn">
+                        <a>
+                            <span><i><Icons.Menu/></i></span>
+                            <span><Icons.Logo />ragneel</span>
+                        </a>
+                    </li>
                     <li>
                         <Link href="/">
-                            <span><i className='fa-solid fa-house'></i></span>
+                            <span><i><Icons.Home/></i></span>
                             <span>Home</span>
                         </Link>
                     </li>
                     <li>
                         <Link href="/discover">
-                            <span><i className="fa-solid fa-compass"></i></span>
+                            <span><i><Icons.Compass/></i></span>
                             <span>Discover</span>
                         </Link>
                     </li>
                     <li>
                         <Link href="/movie">
-                            <span><i className='fa-solid fa-film'></i></span>
+                            <span><i><Icons.Movie/></i></span>
                             <span>Movie</span>
                         </Link>
                     </li>
                     <li>
                         <Link href="/tv">
-                            <span><i className="fa-solid fa-desktop"></i></span>
+                            <span><i><Icons.Tv/></i></span>
                             <span>Tv Show</span>
                         </Link>
                     </li>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,31 @@
 import "./page.module.css"
 export default function Home() {
     return (
+      <div>
+        
       <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>bottom</h1>
+
+    
+      </div>
     )
 }
